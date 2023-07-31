552e5f9efc27e57a96d690e5c0ca181bc45a68dc
hussain <hussain4me2008@gmail.com>
2023-07-31 02:18:12 +0100

    Update files

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -2,15 +2,37 @@ from decimal import Decimal
 from store.models import Product, Collection, Collection
 from rest_framework import serializers
 
-class ProductSerializer(serializers.Serializer):
+
+class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
-    price_with_tax = serializers.SerializerMethodField(method_name="calculate_price")
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset=Collection.objects.all()
-    )
 
 
-    def calculate_tax(self, product: Product ):
-        return product.unit_price * Decimal(1.1)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title','slug', 'inventory', 'unit_price', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    # price_with_tax = serializers.SerializerMethodField()
+    # collection = serializers.PrimaryKeyRelatedField(
+    #     queryset=Collection.objects.all()
+    # )
+    # collection = CollectionSerializer()
+
+    # def get_calculate_tax(self, product: Product ):
+    #     return product.unit_price * Decimal(1.1)
+
+
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+    
+    # def update(self, instance, validated_data):
+    #     instance.unit_price = validated_data.get('unit_price')
+    #     instance.save()
+    #     return instance
--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -8,15 +8,34 @@ from .serializers import ProductSerializer
 
 # Create your views here.
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)   
- 
-@api_view()
+    if request.method == 'GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)  
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        print(serializer.validated_data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif  request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response({'error':'product cannot be deleted because it is associated with order item'}, status = status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
  
